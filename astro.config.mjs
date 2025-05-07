856574866b03f2fc279591d9348e9db172208c85
Axel <axeljpro@gmail.com>
2025-05-07 13:45:16 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cpacademy.fr",
-  output: "static",
+  output: "server",
   integrations: [react()],
   adapter: nodeAdapter({
     mode: "standalone",
